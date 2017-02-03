028808428abe4ff4ac4dea4d3acffb254dd00aa7
mgkett <mgkett@gmail.com>
2017-02-02 18:02:29 -0800

    Added Lesson6 Console as a Library

---
+++ Lesson6ConsoleAsLibrary/Can.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lesson6ConsoleAsLibrary
+{
+    public class Can
+    {
+
+        public readonly Flavor TheFlavor = Flavor.REGULAR;
+
+        public Can()
+        {
+        }
+
+        public Can(Flavor AFlavor)
+        {
+            TheFlavor = AFlavor;
+        }
+
+
+    }
+}
